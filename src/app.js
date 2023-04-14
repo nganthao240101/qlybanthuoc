b6cffd7203637bcdd1dc077b7247104f7f4d4030
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-04-14 23:41:51 +0700

    fix: get token from req.headers

--- src/app.js
+++ src/app.js
@@ -5,15 +5,6 @@ export default {
   setup: () => {
     const app = express();
 
-    // const accessLogStream = rfs("access.log", {
-    //   interval: "1d",
-    //   path: path.join(__dirname, "..", "log"),
-    // });
-    // app.use(logger(config.app.log, { stream: accessLogStream }));
-
-    // app.use(bodyParser.json({ limit: "50mb" }));
-    // app.use(bodyParser.urlencoded({ extended: true }));
-
     // parse application/json
     app.use(express.json());
 
--- src/middleware/allowRoles.js
+++ src/middleware/allowRoles.js
@@ -2,9 +2,12 @@ import { verifyToken } from "../api/auth/verifyToken";
 import { ROLES } from "../constants";
 import { allowRoles } from "../helpers/allowRoles";
 
+const getTokenFromRequest = (req) =>
+  req.headers?.authorization && req.headers?.authorization.split(" ")[1];
+
 export const allowRolesMiddleware = (roles) => {
   return (req, res, next) => {
-    const { token } = req.body;
+    const token = getTokenFromRequest(req);
     const dataDecoded = verifyToken(token);
 
     if (!token && allowRoles(ROLES.GUEST, roles)) {
